4bb6bd9025ca6a537ee3cb266a01f3eaa4add194
Manuel Antonio Novoa Proenza <novoa.pro@gmail.com>
2016-02-18 09:43:22 -0500

    Updated MovieProvider with CRUD methods.

--- app/src/main/java/com/manpdev/androidnanodegree/popularmov/movie/data/provider/MoviesProvider.java
+++ app/src/main/java/com/manpdev/androidnanodegree/popularmov/movie/data/provider/MoviesProvider.java
@@ -1,12 +1,13 @@
 package com.manpdev.androidnanodegree.popularmov.movie.data.provider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -37,17 +38,20 @@ public class MoviesProvider extends ContentProvider{
 
     @Nullable
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
         SQLiteDatabase database = mMovieDatabase.getReadableDatabase();
+        Cursor result;
 
         switch (sUriMatcher.match(uri)){
             case URI_MATCH_MOVIE:
-
+                result = database.query(MovieContract.MovieEntry.TABLE_NAME, MovieContract.MovieEntry.COLUMNS_ALL,
+                                        null, null, null, null, null);
                 break;
 
             case URI_MATCH_MOVIE_ID:
-
+                result = database.query(MovieContract.MovieEntry.TABLE_NAME, MovieContract.MovieEntry.COLUMNS_ALL,
+                        MovieContract.MovieEntry._ID + "=? ", new String[]{uri.getLastPathSegment()}, null, null, null);
                 break;
             default:
                 String message = "Unknown URI: " + uri.toString();
@@ -55,36 +59,102 @@ public class MoviesProvider extends ContentProvider{
                 throw new IllegalArgumentException(message);
         }
 
-        
+        return result;
     }
 
     @Nullable
     @Override
-    public String getType(Uri uri) {
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        SQLiteDatabase database = mMovieDatabase.getReadableDatabase();
+
+        String tableName;
+
+        switch (sUriMatcher.match(uri)){
+            case URI_MATCH_MOVIE:
+                tableName = MovieContract.MovieEntry.TABLE_NAME;
+                break;
+
+            default:
+                String message = "Unknown URI: " + uri.toString();
+                Log.e(TAG, message);
+                throw new IllegalArgumentException(message);
+        }
+
+        long resultId = database.insertWithOnConflict(
+                tableName,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE
+        );
+
+        return ContentUris.withAppendedId(uri, resultId);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        SQLiteDatabase database = mMovieDatabase.getWritableDatabase();
+        int result;
+
+        switch (sUriMatcher.match(uri)){
+            case URI_MATCH_MOVIE:
+                result = database.delete(MovieContract.MovieEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
+
+            case URI_MATCH_MOVIE_ID:
+                result = database.delete(MovieContract.MovieEntry.TABLE_NAME,
+                        MovieContract.MovieEntry._ID + "=? ", new String[]{uri.getLastPathSegment()});
+                break;
+            default:
+                String message = "Unknown URI: " + uri.toString();
+                Log.e(TAG, message);
+                throw new IllegalArgumentException(message);
+        }
+
+        if(getContext() != null && result != 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return result;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        SQLiteDatabase database = mMovieDatabase.getWritableDatabase();
+        int result;
+
+        switch (sUriMatcher.match(uri)){
+            case URI_MATCH_MOVIE:
+                result = database.update(MovieContract.MovieEntry.TABLE_NAME, values,
+                        selection, selectionArgs);
+                break;
+
+            case URI_MATCH_MOVIE_ID:
+                result = database.update(MovieContract.MovieEntry.TABLE_NAME, values,
+                        MovieContract.MovieEntry._ID + "=? ", new String[]{uri.getLastPathSegment()});
+                break;
+            default:
+                String message = "Unknown URI: " + uri.toString();
+                Log.e(TAG, message);
+                throw new IllegalArgumentException(message);
+        }
+
+        if(getContext() != null && result != 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return result;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
         switch (sUriMatcher.match(uri)){
             case URI_MATCH_MOVIE:
                 return MovieContract.MovieEntry.CONTENT_TYPE;
             case URI_MATCH_MOVIE_ID:
                 return MovieContract.MovieEntry.CONTENT_TYPE_ITEM;
+            default:
+                return null;
         }
-
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
-    }
-
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
-    }
-
-    @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
     }
 
     private void initUriMatcher() {

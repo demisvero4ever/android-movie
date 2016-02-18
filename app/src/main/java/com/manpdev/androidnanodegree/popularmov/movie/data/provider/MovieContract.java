ba4e036b2ea9b27f501e06daffb2a8d254448d8c
Manuel Antonio Novoa Proenza <novoa.pro@gmail.com>
2016-02-18 00:00:30 -0500

    Finished movie contract.

--- app/src/main/java/com/manpdev/androidnanodegree/popularmov/movie/data/provider/MovieContract.java
+++ app/src/main/java/com/manpdev/androidnanodegree/popularmov/movie/data/provider/MovieContract.java
@@ -1,5 +1,7 @@
 package com.manpdev.androidnanodegree.popularmov.movie.data.provider;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,19 +11,38 @@ public class MovieContract {
 
     public static final class MovieEntry implements BaseColumns{
 
-        public static final String TABLE_NAME = "pm_movie";
-
+        public static final String TABLE_NAME = "movie";
         public static final String COLUMN_CLOUD_ID = "cloud_id";
-
         public static final String COLUMN_TITLE = "title";
-
         public static final String COLUMN_RELEASE_DATE = "release_date";
-
         public static final String COLUMN_POSTER = "poster";
-
         public static final String COLUMN_VOTE_AVERAGE = "vote_avg";
-
         public static final String COLUMN_SYNOPSIS = "synopsis";
 
+        public static String[] COLUMNS_ALL = {_ID, COLUMN_CLOUD_ID, COLUMN_TITLE,
+                COLUMN_POSTER, COLUMN_SYNOPSIS, COLUMN_VOTE_AVERAGE, COLUMN_RELEASE_DATE};
+
+        public static final String CONTENT_TYPE = getDirBaseType(TABLE_NAME);
+        public static final String CONTENT_TYPE_ITEM = getItemBaseType(TABLE_NAME);
+
+        public static Uri baseURI(String authority){
+            return getBaseContentUri(authority, TABLE_NAME);
+        }
+
+        public static Uri buildUriForMovieById(String authority, int movieId){
+            return Uri.withAppendedPath(baseURI(authority), String.valueOf(movieId));
+        }
+    }
+
+    private static Uri getBaseContentUri(String authority, String basePath) {
+        return Uri.parse(ContentResolver.SCHEME_CONTENT + "://" + authority + "/" + basePath);
+    }
+
+    private static String getDirBaseType(String type) {
+        return ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + type;
+    }
+
+    private static String getItemBaseType(String type) {
+        return ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + type;
     }
 }
--- app/src/main/java/com/manpdev/androidnanodegree/popularmov/movie/data/provider/MoviesProvider.java
+++ app/src/main/java/com/manpdev/androidnanodegree/popularmov/movie/data/provider/MoviesProvider.java
@@ -1,7 +1,101 @@
 package com.manpdev.androidnanodegree.popularmov.movie.data.provider;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.manpdev.androidnanodegree.popularmov.movie.data.db.MovieDatabase;
+
 /**
  * Created by novoa.pro@gmail.com on 2/14/16
  */
-public class MoviesProvider {
+public class MoviesProvider extends ContentProvider{
+
+    private static final String TAG = "MoviesProvider";
+    public static final String sAUTHORITY = "com.manpdev.androidnanodegree.popularmov.movie";
+
+    private static final int URI_MATCH_MOVIE = 0;
+    private static final int URI_MATCH_MOVIE_ID = 1;
+
+    private static UriMatcher sUriMatcher;
+    private MovieDatabase mMovieDatabase;
+
+    @Override
+    public boolean onCreate() {
+        initUriMatcher();
+
+        this.mMovieDatabase = new MovieDatabase(getContext());
+
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        SQLiteDatabase database = mMovieDatabase.getReadableDatabase();
+
+        switch (sUriMatcher.match(uri)){
+            case URI_MATCH_MOVIE:
+
+                break;
+
+            case URI_MATCH_MOVIE_ID:
+
+                break;
+            default:
+                String message = "Unknown URI: " + uri.toString();
+                Log.e(TAG, message);
+                throw new IllegalArgumentException(message);
+        }
+
+        
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        switch (sUriMatcher.match(uri)){
+            case URI_MATCH_MOVIE:
+                return MovieContract.MovieEntry.CONTENT_TYPE;
+            case URI_MATCH_MOVIE_ID:
+                return MovieContract.MovieEntry.CONTENT_TYPE_ITEM;
+        }
+
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    private void initUriMatcher() {
+        if(sUriMatcher != null)
+            return;
+
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        //../movie
+        sUriMatcher.addURI(sAUTHORITY, MovieContract.MovieEntry.TABLE_NAME, URI_MATCH_MOVIE);
+        //../movie/{movie_id}
+        sUriMatcher.addURI(sAUTHORITY, MovieContract.MovieEntry.TABLE_NAME + "/*", URI_MATCH_MOVIE_ID);
+    }
 }
